ddd5f70aff49274f5e5179228c76701b17e8a03c
Guenter Schafranek <gschafra@web.de>
2016-06-22 16:33:16 +0200

    Added .gitignore, views and added socket.io packages

--- app.js
+++ app.js
@@ -1,8 +1,14 @@
 var express = require('express');
 var app = express();
+var server = require('http').createServer(app);
+var io = require('socket.io')(server);
 
-app.get('/', function(request, response) {
-  response.send('Hello world');
+io.on('connection', function(client){
+  console.log('Socket client connected...');
+});
+app.set('view engine', 'ejs');
+app.get('/', function(req, res) {
+  res.render('pages/index');
 }).listen(3000, function(){
   console.log('Express app listing...');
 });
