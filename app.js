8e482051d8b5d5cd75f6c8f3e83d0613ebd6ddb2
Guenter Schafranek <gschafra@web.de>
2016-06-24 22:14:03 +0200

    Continuing chatter app

--- app.js
+++ app.js
@@ -4,11 +4,15 @@ var server = require('http').createServer(app).listen(3000);
 var io = require('socket.io').listen(server);
 
 io.on('connection', function(client){
-  console.log('Socket client connected...'); 
+	//socket.broadcast.emit('')
+	client.on('post message', function(msg){
+		console.log('Message: ' + msg);
+	});
+	console.log('Socket client connected...');
 });
 app.set('view engine', 'ejs');
 app.get('/', function(req, res) {
-  res.render('pages/index');
+	res.render('pages/index');
 });
 /*.listen(3000, function(){
   console.log('Express app listing...');
